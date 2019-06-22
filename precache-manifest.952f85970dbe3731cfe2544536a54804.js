9e36dd3cebe62134b7b567c0c7e92462558948e7
Ephraim <ephraim.aigbefo@outlook.com>
2019-06-22 13:40:12 +0100

    Updates

---
+++ precache-manifest.952f85970dbe3731cfe2544536a54804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50b00d8f1d37d931545187b4b71e133",
+    "url": "/mars-photo-search/index.html"
+  },
+  {
+    "revision": "58331efc92662cf2c06c",
+    "url": "/mars-photo-search/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b6a3d85c6cd2835fd8ef",
+    "url": "/mars-photo-search/static/css/main.bd8df0e4.chunk.css"
+  },
+  {
+    "revision": "58331efc92662cf2c06c",
+    "url": "/mars-photo-search/static/js/2.ee1be4a1.chunk.js"
+  },
+  {
+    "revision": "b6a3d85c6cd2835fd8ef",
+    "url": "/mars-photo-search/static/js/main.e0922a00.chunk.js"
+  },
+  {
+    "revision": "1715b8faf2d9386b3522",
+    "url": "/mars-photo-search/static/js/runtime~main.ce0302a5.js"
+  }
+]);
